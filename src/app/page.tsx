4c0879e102e2af4f2a1f03e9047a4c5715ed06c9
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-10-01 23:30:27 +0500

    added landscape treemap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { NavBar } from '@/components/NavBar'
 import HomePage from '@/components/HomePage'
 import Challenges from '@/components/Challenges'
@@ -8,20 +10,39 @@ import RwaResearch from '@/components/RwaResearch'
 import RwaLandscape from '@/components/RwaLandscape'
 import BusinessDevelopment from '@/components/BusinessDevelopment'
 import Consultations from '@/components/Consultations'
+import { Treemap } from '@/components/TreeMap'
+import { data } from './data'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 export default function Home() {
+  const width = window.innerWidth
+  const height = window.innerHeight
+
   return (
-    <>
-      <HomePage />
-      <NavBar />
-      <Challenges />
-      <RwaSolutions />
-      <RwaResearch />
-      <RwaLandscape />
-      <BusinessDevelopment />
-      <Consultations />
-      <ContactPage />
-      <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <HomePage />
+              <NavBar />
+              <Challenges />
+              <RwaSolutions />
+              <RwaResearch />
+              <RwaLandscape />
+              <BusinessDevelopment />
+              <Consultations />
+              <ContactPage />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/landscape"
+          element={<Treemap data={data} width={width * 0.9} height={height} />}
+        />
+      </Routes>
+    </Router>
   )
 }
