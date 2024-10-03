3b007ca9ff0243901c7e17fed07add234c9c802c
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-10-03 20:50:56 +0500

    remove commented code

--- src/app/landscape/page.jsx
+++ src/app/landscape/page.jsx
@@ -1,39 +1,11 @@
 'use client'
 
-import { Treemap } from '@/components/TreeMap'
 import { data } from '@/app/data'
-import { useEffect, useState } from 'react'
 import EcosystemMap from '@/components/ReactTreeMap'
 
 export default function LandscapePage() {
-  // const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
-
-  // useEffect(() => {
-  //   const updateDimensions = () => {
-  //     setDimensions({
-  //       width: window.innerWidth,
-  //       height: window.innerHeight,
-  //     })
-  //   }
-
-  //   updateDimensions()
-  //   window.addEventListener('resize', updateDimensions)
-
-  //   return () => window.removeEventListener('resize', updateDimensions)
-  // }, [])
-
-  // // Render nothing until dimensions are set to avoid SSR issues
-  // if (dimensions.width === 0 || dimensions.height === 0) {
-  //   return null
-  // }
-
   return (
     <div className="flex justify-center">
-      {/* <Treemap
-        data={data}
-        width={dimensions.width * 0.9}
-        height={dimensions.height}
-      /> */}
       <EcosystemMap data={data.children} />
     </div>
   )
